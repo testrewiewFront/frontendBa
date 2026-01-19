c3262701ff0aafd1d95dc0f83169e183fc996bbb
testrewiewFront <startlk0191@gmail.com>
2026-01-19 17:25:20 +0200

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL:  "https://api.international-payments.cc",
+  baseURL:  "https://api.international-payments.cc/api",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',

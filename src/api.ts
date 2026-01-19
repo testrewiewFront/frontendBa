2d23bf584563dd1f997418df2a594c8a4a783bae
testrewiewFront <startlk0191@gmail.com>
2026-01-19 17:25:03 +0200

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_BASE_URL,
+  baseURL:  "https://api.international-payments.cc",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
